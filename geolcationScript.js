93f4038278e7758e2ebc004280cc562850c465a2
Mattie Gonzalez <gonzalem@lssc.edu>
2022-11-10 21:44:24 -0500

    Geolocation update

--- geolcationScript.js
+++ geolcationScript.js
@@ -1,28 +1,43 @@
-//Get User Location
-//User Must Allow Location to sign in?? Ask Group 
-const successCallback = (position) => {
-  console.log(successCallback)
-}
-const errorCallback = (position) => {
-  
-}
+// //Get User Location
+// //User Must Allow Location to sign in?? Ask Group 
 
-const options = {
-  enableHighAccuracy: true,
-  timeout: 5000,
-}
-navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+// if ('geolocation' in navigator) {
+//   console.log('geolocation is available');
+//   navigator.geolocation.getCurrentPosition(position => {
+//   console.log(position.coords.latitude);
+//   console.log(position.coords.longitude);
+//     });
 
+// } else {
+//   console.log('geolocation IS NOT available');
+// }
 
-function initMap(){
+//Get Employee Location
+function employeeLocation() {
 
-  //Searches for location 
+  const location = document.querySelector('#location');
+  const mapLocation = document.querySelector('#map-location');
 
-  var options = {
-    center: {lat: 38.3460, lng: -0.4907},
-    zoom: 10
+  mapLocation.href = '';
+  mapLocation.textContent = '';
+
+  //Generating Users current Position (Latitude and Longitude Coordinates)
+  function current(position) {
+    const latitude  = position.coords.latitude;
+    const longitude = position.coords.longitude;
+
+    //Console Loggin Coordinates Aswell
+    console.log(position.coords.latitude);
+    console.log(position.coords.longitude);
+
+    //Plotting Employee Location (Lat/Lon) On a Map
+    location.textContent = '';
+    mapLocation.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+    mapLocation.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+  }
+    navigator.geolocation.getCurrentPosition(current);
   }
 
-  //Displays location
-  map = new google.maps.Map(document.getElementById('map'), options)
-}
+//When Employee Clicks their Lat/Lon Coordinates are presented
+//When Employee Clicks on their Lat/Lon Coordinates they are directed to map of their location
+document.querySelector('#employee-location').addEventListener('click', employeeLocation);
