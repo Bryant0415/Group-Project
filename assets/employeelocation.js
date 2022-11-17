91a326c9851952737ddc5e3ee2d8b626ce5f639d
Mattie Gonzalez <gonzalem@lssc.edu>
2022-11-16 19:55:13 -0500

    Geolocation Page Display

--- assets/employeelocation.js
+++ assets/employeelocation.js
@@ -1,3 +1,4 @@
+//Get Employee Current Position in Lat/Lng Coordinates
 navigator.geolocation.getCurrentPosition(
     function (position) {
         employeeLocation(position.coords.latitude, position.coords.longitude);
@@ -7,17 +8,19 @@ navigator.geolocation.getCurrentPosition(
             lng,
         };
 
+        //Opening Google Maps Api/ Plotting Employees Location
         var map = new google.maps.Map(document.getElementById("location"), {
         zoom: 15,
         center: employeeLatLng,
         });
         
+        //Marker for Google Maps
         var marker = new google.maps.Marker({
         position: employeeLatLng,
         map: map,
         });
 };
-         //Console Loggin Coordinates Aswell
+         //Coordinates are presented in the Console Log
         console.log(position.coords.latitude);
         console.log(position.coords.longitude);
     }
